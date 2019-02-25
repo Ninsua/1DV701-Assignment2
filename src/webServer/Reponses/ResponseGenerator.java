86ffedf38521320bdf70a948d40689cc8e29fd96
Ninsua <e@anjel.se>
2019-02-25 15:11:10 +0100

    Fixed the folder to index.html redirect. Implemented use of the ResponseGenerator.

--- src/webServer/Reponses/ResponseGenerator.java
+++ src/webServer/Reponses/ResponseGenerator.java
@@ -1,31 +1,47 @@
 package webServer.Reponses;
 
 public class ResponseGenerator {
-
     private StringBuilder responseHeader;
     private String status;
     private ContentType contentType;
 
-
-    public ResponseGenerator(StatusCodes status, ContentType type) {
+    public ResponseGenerator(StatusCodes status, String type) {
         this.status = status.getStatus();
-        this.contentType = type;
+        this.contentType = fileExtensionToContentType(type);
         responseHeader = new StringBuilder();
         generateResponseHeader();
     }
-
 
     private void generateResponseHeader() {
         this.responseHeader.append("HTTP/1.1 ").append(status).append("\n")
                 .append("Server: YourWorstNightmare 0.1\n")
                 // .append("Content-length: ").append(contentLength).append("\n")
                 .append("Connection: close\n")
-                .append("Content-Type: ").append(contentType).append("; charset=UTF-8\n\n");
+                .append("Content-Type: ")
+                .append(contentType.getType()).append(";");
+                
+        if (!isImg())
+        	this.responseHeader.append(" charset=UTF-8");
+
+        this.responseHeader.append("\n\n");           
     }
 
-    private String getResponseHeader() {
+    public String getResponseHeader() {
         return responseHeader.toString();
     }
-
+    
+    private boolean isImg() {
+    	return contentType.getType().contains("image");
+    }
+    
+    private ContentType fileExtensionToContentType(String type) throws IllegalArgumentException {
+    	type = type.toUpperCase();
+    	
+    	for (ContentType contentType : ContentType.values()) {
+    		if (contentType.toString().equals(type))
+    			return contentType;
+    	}
+    	throw new IllegalArgumentException();
+    }
 
 }
